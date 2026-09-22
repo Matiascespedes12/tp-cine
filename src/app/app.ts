c31c629ded2c99e880a067de0d24aa2c825ee65c
matiascespedes12 <matiasojedaa10@gmail.com>
2026-09-21 22:26:33 -0300

    Conexion con Supabase funcionando, datos de prueba cargados

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,27 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Supabase } from './core/supabase';
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
-  styleUrl: './app.scss',
+  imports: [RouterOutlet],
   templateUrl: './app.html',
+  styleUrl: './app.scss'
 })
-export class App {
-  protected readonly title = signal('tp-cine');
+export class App implements OnInit {
+  protected title = 'tp-cine';
+
+  constructor(private supabase: Supabase) {}
+
+  async ngOnInit() {
+    const { data, error } = await this.supabase.client
+      .from('peliculas')
+      .select('*');
+
+    if (error) {
+      console.error('Error al conectar con Supabase:', error);
+    } else {
+      console.log('Conexión exitosa. Películas encontradas:', data);
+    }
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://emqlewxdokcqvrtoeuiq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVtcWxld3hkb2tjcXZydG9ldWlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MjE0MTMsImV4cCI6MjEwNDk5NzQxM30.Ce01zE7QAPKEODf1OmHCP1TWatkOk6Dw7cOQ_nWX-g4'
+};
